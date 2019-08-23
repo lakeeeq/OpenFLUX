aa5fc0b59db5b04eece0b3e3eb073f281cb2959f
Lake-Ee Quek <44207836+lakeeeq@users.noreply.github.com>
2019-08-23 23:06:01 +1000

    Update OFstartHere_2009.m

--- OFstartHere_2009.m
+++ OFstartHere_2009.m
@@ -1,5 +1,7 @@
 clearvars
 clc
+%%%run this script to create files produced previously by JAVA
+%%%files are created at the same path as the input model
 addpath OFfunctions %can be added to MATLAB startup
 
 OF = OpenFLUX;
